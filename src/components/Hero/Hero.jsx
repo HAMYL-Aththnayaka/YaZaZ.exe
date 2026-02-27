2ee1d69fed3fb4b2a38ae4d9910b7b5b153ce0a9
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-02-27 23:57:28 +0530

    Hero page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,18 @@ import React from "react"
 
 const Hero =()=>{
     return(<>
+    <main>
+        <div className="container">
+            {/**Text content section */}
+            <div>
+
+            </div>
+            {/**image content section */}
+            <div>
+                
+            </div>
+        </div>
+    </main>
     </>);
 }
 export default Hero
