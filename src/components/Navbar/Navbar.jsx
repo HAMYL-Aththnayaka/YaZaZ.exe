2043a6ee1dc9080960d91b95610d428ae2cfe949
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-02-27 23:45:01 +0530

    Navbar, Tailwind setup and styling updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -33,28 +33,32 @@ const Navbar = () => {
             <div className="container mx-auto flex justify-between items-center">
                 
                 {/* Logo Section */}
-                <div>
-                    <img src={logo} alt="Logo" className="h-10" />
-                </div>
+                <div className="py-5 bg-grayCustom text-white shadow-sm">
+  <div className="container mx-auto flex justify-between items-center">
+    
+    {/* Logo */}
+    <div>
+      <img src={logo} alt="Logo" className="h-10" />
+    </div>
 
-                {/* Nav Menu Section */}
-                <div>
-                    <ul className="flex justify-center gap-8 font-medium">
-                        {NavMenu.map((item) => (
-                            <li key={item.id}>
-                                <a 
-                                    href={item.link}
-                                    className="hover:text-primary transition"
-                                >
-                                    {item.title}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+    {/* Menu */}
+    <div>
+      <ul className="flex justify-center gap-8 font-medium">
+        {NavMenu.map((item) => (
+          <li key={item.id}>
+            <a 
+              href={item.link}
+              className="hover:text-primary transition"
+            >
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
 
-            </div>
-        </div>
+  </div>
+</div>
     );
 };
 
