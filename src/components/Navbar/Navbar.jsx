bb5fbf51701f71e6a18a9495b567440e483ed2c1
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-02-27 23:45:24 +0530

    Navbar bug fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,64 +1,41 @@
-import React from 'react'
-import logo from '../../assets/Logo.PNG'
+import React from 'react';
+import logo from '../../assets/Logo.PNG';
 
 const NavMenu = [
-    {
-        id: 1,
-        title: "Services",
-        link: '#'
-    },
-    {
-        id: 2,
-        title: "Works",
-        link: '#'
-
-    },
-    {
-        id: 3,
-        title: "Blog",
-        link: '#'
-
-    },
-    {
-        id: 4,
-        title: "About Me",
-        link: '#'
-    },
-
-
+    { id: 1, title: "Services", link: '#' },
+    { id: 2, title: "Works", link: '#' },
+    { id: 3, title: "Blog", link: '#' },
+    { id: 4, title: "About Me", link: '#' },
 ];
+
 const Navbar = () => {
     return (
-        <div className="py-5 bg-[color:theme('colors.gray')] text-white shadow-sm">
+        <div className="py-5 bg-grayCustom text-white shadow-sm">
             <div className="container mx-auto flex justify-between items-center">
                 
                 {/* Logo Section */}
-                <div className="py-5 bg-grayCustom text-white shadow-sm">
-  <div className="container mx-auto flex justify-between items-center">
-    
-    {/* Logo */}
-    <div>
-      <img src={logo} alt="Logo" className="h-10" />
-    </div>
+                <div>
+                    <img src={logo} alt="Logo" className="h-10" />
+                </div>
 
-    {/* Menu */}
-    <div>
-      <ul className="flex justify-center gap-8 font-medium">
-        {NavMenu.map((item) => (
-          <li key={item.id}>
-            <a 
-              href={item.link}
-              className="hover:text-primary transition"
-            >
-              {item.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+                {/* Nav Menu Section */}
+                <div>
+                    <ul className="flex justify-center gap-8 font-medium">
+                        {NavMenu.map((item) => (
+                            <li key={item.id}>
+                                <a 
+                                    href={item.link} 
+                                    className="hover:text-primary transition"
+                                >
+                                    {item.title}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-  </div>
-</div>
+            </div>
+        </div>
     );
 };
 
