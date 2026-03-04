b5700e5592bab1cab29c16edc75c0cccbce3be53
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-03-04 23:46:56 +0530

    hero page updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,27 @@
-import React from "react"
+import React from "react";
+import me from "../../assets/Me.png"
 
-const Hero =()=>{
-    return(<>
-    <main>
-        <div className="container">
-            {/**Text content section */}
+const Hero = () => {
+  return (
+    <>
+      <main className="bg-gray-900 text-white">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-2">
             <div>
-
+              <h1 className="text-6xl font-bold">Yasas Aththanayaka</h1>
+              <div className="h-[4px] w-[30px] bg-blue-600"></div>
             </div>
             {/**image content section */}
             <div>
-                
+                <img src={me} alt='' className="" />
             </div>
+          </div>
+          {/**Text content section */}
+          <div></div>
         </div>
-    </main>
-    </>);
-}
-export default Hero
+      </main>
+    </>
+  );
+};
+
+export default Hero;
